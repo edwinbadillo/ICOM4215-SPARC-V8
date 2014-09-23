320577d975acec61ef52d5e48ee978a041073a66
Rafael Pol <rafael.pol1@upr.edu>
2014-09-23 16:12:11 -0400

    Testing and implementation of mul and div are needed

--- ALU.v
+++ ALU.v
@@ -1,6 +1,7 @@
-module alu(output reg [31:0]res, N, Z, V, C,[31:0]yReg,input [5:0]op, [31:0]a, [31:0]b, Cin);
+module alu(output reg [31:0]res, [31:0]yRegO, N, Z, V, C, trap,input wire [5:0]op, [31:0]a, [31:0]b, [31:0]yRegI, Cin);
 reg carry;
-reg CC = 5'h10;
+parameter CC = 5'h10;
+integer temp;
 always @ (op,a,b,Cin)
 begin
 casex (op)
@@ -28,12 +29,11 @@ casex (op)
 		if(CC && op)
 			// Modify cc
 			begin
-			// Carry flag is cleared in logical instrucions
+			// Carry flag and overflag is cleared in logical instrucions
+			V = 0;
 			C = 0;
 			// Negative flag
 			N = res[31];
-			// Overflow is cleared
-			V = 0;
 			// Zero flag
 			Z = (res == 0);
 			end
@@ -45,12 +45,11 @@ casex (op)
 		if(CC && op)
 			// Modify cc
 			begin
-			// Carry flag is cleared in logical instrucions
+			// Carry flag and overflag is cleared in logical instrucions
+			V = 0;
 			C = 0;
 			// Negative flag
 			N = res[31];
-			// Overflow is cleared
-			V = 0;
 			// Zero flag
 			Z = (res == 0);
 			end
@@ -62,12 +61,11 @@ casex (op)
 		if(CC && op)
 			// Modify cc
 			begin
-			// Carry flag is cleared in logical instrucions
+			// Carry flag and overflag is cleared in logical instrucions
+			V = 0;
 			C = 0;
 			// Negative flag
 			N = res[31];
-			// Overflow is cleared
-			V = 0;
 			// Zero flag
 			Z = (res == 0);
 			end
@@ -96,50 +94,50 @@ casex (op)
 		if(CC && op)
 			// Modify cc
 			begin
-			// Carry flag is cleared in logical instrucions
+			// Carry flag and overflag is cleared in logical instrucions
+			V = 0;
 			C = 0;
 			// Negative flag
 			N = res[31];
-			// Overflow is cleared
-			V = 0;
 			// Zero flag
 			Z = (res == 0);
 			end
 		end
 	// ORN (6 + 22)
 	6'b0?0110:
+		begin
 		res = a | ~b; //Not is applied to the second operand
-`		if(CC && op)
+		if(CC && op)
 			// Modify cc
 			begin
-			// Carry flag is cleared in logical instrucions
+			// Carry flag and overflag is cleared in logical instrucions
+			V = 0;
 			C = 0;
 			// Negative flag
 			N = res[31];
-			// Overflow is cleared
-			V = 0;
 			// Zero flag
 			Z = (res == 0);
 			end
 		end
 	// XNOR (7 y 23)
 	6'b0?0111:
+		begin
 		res = a ^ ~b; //Not is applied to the second operand
 		if(CC && op)
 			// Modify cc
 			begin
-			// Carry flag is cleared in logical instrucions
+			// Carry flag and overflag is cleared in logical instrucions
+			V = 0;
 			C = 0;
 			// Negative flag
 			N = res[31];
-			// Overflow is cleared
-			V = 0;
 			// Zero flag
 			Z = (res == 0);
 			end
 		end
 	// ADDX (8 y 24)
 	6'b0?1000:
+		begin
 		{carry,res} = a + b + Cin;
 		if(CC && op)
 			// Modify cc
@@ -156,47 +154,162 @@ casex (op)
 		end
 	// UMUL (10 y 26) Multiplication results in a 64 bit result divided in res (lsb) and the Y register (msb)
 	6'b0?1010:
-		{yReg, res} = a * b;
+		begin
+		{yRegO, res} = a * b;
+		if(CC && op)
+			// Modify cc
+			begin
+			// Clear carry flag and overflow flag
+			C = 0;
+			V = 0;
+			// Negative flag
+			N = res[31];
+			// Zero flag
+			Z = (res == 0);
+			end
+		end
+	/* TO DO SIGN EXTENSION */
+	/* TO DO SIGN EXTENSION */
 	/* TO DO SIGN EXTENSION */
 	// SMUL (11 y 27) Multiplication results in a 64 bit result divided in res (lsb) and the Y register (msb)
 	6'b0?1011:
 		begin
-		{yReg, res} = a * b;
+		{yRegO, res} = a * b;
+		if(CC && op)
+			// Modify cc
+			begin
+			// Clear carry flag and overflow flag
+			C = 0;
+			V = 0;
+			// Negative flag
+			N = res[31];
+			// Zero flag
+			Z = (res == 0);
+			end
 		end
 	// SUBX (12 y 28)
 	6'b0?1100:
-		{yReg, res} = a * b;
-	/* TO DO */
+		begin
+		{carry, res} = a - b - Cin;
+		if(CC && op)
+			// Modify cc
+			begin
+			// Carry flag
+			C = carry;
+			// Negative flag
+			N = res[31];
+			// Overflow => Num(+) - Num(-) = Num(-) o Num(-) - Num(+) = Num(+)
+			V = ((a[31] != b[31]) && (a[31] != res[31]));
+			// Zero flag
+			Z = (res == 0);
+			end
+		end
+	/* TO DO SIGN EXTENSION */
+	/* TO DO SIGN EXTENSION */
+	/* TO DO SIGN EXTENSION */
 	// UDIV (14 y 30)
 	6'b0?1110:
-		res = a / b;
+		// Concatinate yReg and a to form a 64 bit number to be divided by b which results
+		// in a 32 bit number
+		res = {yRegI, a} / b;
+	/* TO DO SIGN EXTENSION */
+	/* TO DO SIGN EXTENSION */
+	/* TO DO SIGN EXTENSION */
 	// SDIV (15 y 31)
 	6'b0?1111:	
 		res = a / b;
 	// TADcc (32)
-	6'b100000:	
-		res = a / b;
+	// tag overflow occurs if bit 1 or bit 0 of either operand is nonzero, or if the 
+	// addition generates an arithmetic overflow
+	6'b100000:
+		begin
+		{C,res} = a + b;
+		// Tag overflow occured
+		if( (a[1:0] != 0 || b[1:0] != 0) || ( (a[31] == b[31]) && (res[31] != a[31])))
+			V = 1;
+		else
+			V = 0;
+		// Negative flag
+		N = res[31];
+		// Zero flag
+		Z = (res == 0);
+		end
 	// TSUBcc (33)
 	6'b100001:	
-		res = a / b;
+		begin
+		{C,res} = a - b;
+		// Tag overflow occured
+		if( (a[1:0] != 0 || b[1:0] != 0) || (a[31] != b[31] && a[31] != res[31]))
+			V = 1;
+		else
+			V = 0;
+		// Negative flag
+		N = res[31];
+		// Zero flag
+		Z = (res == 0);
+		end
 	// TADDccTV (34)
-	6'b100010:	
-		res = a / b;
+	// Generates trap if a tag overflow occurs, operation is cancelled
+	6'b100010:
+		begin
+		{carry, temp} = a + b;
+		// Tag overflow occured
+		if( (a[1:0] != 0 || b[1:0] != 0) || (a[31] == b[31] && temp[31] != a[31]))
+			// Trap generated
+			trap = 1;
+		else
+			begin
+			// restore value
+			res = temp;
+			// Modify cc
+			V = 0;
+			C = carry;
+			// Negative flag
+			N = res[31];
+			// Zero flag
+			Z = (res == 0);
+			end
+		end
 	// TSUBccTV (35)
+	// Generates trap if a tag overflow occurs, operation is cancelled
 	6'b100011:	
-		res = a / b;
+		begin
+		{carry, temp} = a - b;
+		// Tag overflow occured
+		if( (a[1:0] != 0 || b[1:0] != 0) || (a[31] != b[31] && a[31] != res[31]))
+			// Trap generated
+			trap = 1;
+		else
+			begin
+			// restore value
+			res = temp;
+			// Modify cc
+			V = 0;
+			C = carry;
+			// Negative flag
+			N = res[31];
+			// Zero flag
+			Z = (res == 0);
+			end
+		end
 	// MULScc (36)
-	6'b100100:	
-		res = a / b;
-	// SLL (37)
-	6'b100101:	
-		res = a / b;
+	// no entiendo
+	6'b100100:
+		begin
+		//a = a >> 1;
+		//a = a | (N ^ V);
+		//if(yRegI[0] == 1)
+		end
+	// SLL (37) shift count is giving by the 5 lsb of r[rs2]
+	6'b100101:
+		// Ignore the 27 MSB of r[rs2]
+		res = a << (b & 5'b11111);
 	// SRL (38)
 	6'b100110:	
-		res = a / b;
+		res = a >> (b & 5'b11111);
 	// SRA (39)
-	6'b100111:	
-		res = a / b;
+	6'b100111:
+		res = a >>> (b & 5'b11111);
 endcase
 end
 endmodule
