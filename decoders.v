a5b8ee54ebc98f3a3b0b30afb235b58d73b4a4a6
Carlos N. Abreu Takemura <currysensei@outlook.com>
2014-09-28 18:57:40 -0400

    Added 2x4 and 5x32 Decoders to be used for the register file to help implement register windows.

---
+++ decoders.v
@@ -0,0 +1,13 @@
+// Decoders
+
+// 2x4 Decoder
+module decoder_2x4(output reg [3:0]out, input wire [1:0]in, input wire enable);
+// If enabled, it chooses the corresponding wire by shifting a 1 'in' times to the left
+assign out = (enable) ? (1 << in) : 4'b0;
+endmodule
+
+// 5x32 Decoder
+module decoder_5x32(output reg [31:0]out, input wire [4:0]in, input wire enable);
+// If enabled, it chooses the corresponding wire by shifting a 1 'in' times to the left
+assign out = (enable) ? (1 << in) : 32'b0;
+endmodule
--- register_file.v
+++ register_file.v
@@ -1,1 +1,3 @@
 // Register file
+
+module register_file(output reg [31:0] out, input [31:0] in, input enable, Clr, Clk); // still missing some arguments
