dcd26f2cb155bef359daf47e037f638fead1e9af
Rafael Pol <rafael.pol1@upr.edu>
2014-09-28 15:15:03 -0400

    ops

--- register_32.v
+++ register_32.v
@@ -1,5 +1,5 @@
 module register_32 (output reg [31:0] out, input [31:0] in, input enable, Clr, Clk);
 always @ (posedge Clk, negedge Clr)
-	if (!Clr) Out <= 32'h00000000;
+	if (!Clr) out <= 32'h00000000;
 	else if (!enable) out <= in;
 endmodule
