839f9fd706e8ae81b6d8db734270cc255b8bc983
Carlos N. Abreu Takemura <currysensei@outlook.com>
2014-09-29 12:43:32 -0400

    Fixed an error with decoders.

--- decoders.v
+++ decoders.v
@@ -3,11 +3,11 @@
 // 2x4 Decoder
 module decoder_2x4(output reg [3:0]out, input wire [1:0]in, input wire enable);
 // If enabled, it chooses the corresponding wire by shifting a 1 'in' times to the left
-assign out = (enable) ? (1 << in) : 4'b0;
+assign out = (enable) ? (4'b1 << in) : 4'b0;
 endmodule
 
 // 5x32 Decoder
 module decoder_5x32(output reg [31:0]out, input wire [4:0]in, input wire enable);
 // If enabled, it chooses the corresponding wire by shifting a 1 'in' times to the left
-assign out = (enable) ? (1 << in) : 32'b0;
+assign out = (enable) ? (32'b1 << in) : 32'b0;
 endmodule
--- register_file.v
+++ register_file.v
@@ -1,3 +1,13 @@
 // Register file
 
-module register_file(output reg [31:0] out, input [31:0] in, input enable, Clr, Clk); // still missing some arguments
+module register_file(output reg [31:0] out, input [31:0] in, input enable, Clr, Clk, current_window); // still missing some arguments
+
+
+decoder_2x4()
+
+
+
+//DFF d[15:0] (clk, DFF_i, DFF_o);
+
+
+endmodule
