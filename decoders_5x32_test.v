dded75664d068980d9545251c4f385140596a3d7
Rafael Pol <rafael.pol1@upr.edu>
2014-09-29 15:09:10 -0400

    Fixed decoders declarations and added test modules for the decoders. Added a semicolon in memory

--- decoders_5x32_test.v
+++ decoders_5x32_test.v
@@ -1,16 +1,16 @@
-module test_decoder_2x4;
+module test_decoder_5x32;
 
 	// Inputs
-	reg [1:0]in;
+	reg [4:0]in;
 	reg enable; 
 
 	// Outputs
-	wire [3:0]out;
+	wire [31:0]out;
 
 	// Constants
-	parameter sim_time = 40;
+	parameter sim_time = 160;
 
-	decoder_2x4 decoder (out,in,enable);
+	decoder_5x32 decoder (out,in,enable);
 
 	// End simulation at sim_time
 	initial #sim_time $finish;
@@ -19,18 +19,25 @@ module test_decoder_2x4;
 	initial 
 	begin
 		in = 0;
+		enable = 1;
 		$display("In \t Out \t Enable");
 		$monitor ("%b \t %b \t %b",in, out, enable);
 	end
 
+	// Change input each 5ns interval
 	initial 
 	begin
-		repeat(8)
+		repeat(32)
 		begin
 			#5 in = in + 1;
-			#30 enable = 0;
 		end
-		
 	end
 
+	// Disable decoder at 30ns and enable decoder at 40ns
+	initial 
+	begin
+		#30 enable = 0;
+		#10 enable = 1;
+	end
+	
 endmodule
