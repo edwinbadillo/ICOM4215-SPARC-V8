445c16cc341da3c208537e1d3b18f24fa023a36c
Edwin O. Badillo <edwin.o.badillo@ieee.org>
2014-10-06 22:22:32 -0400

    Updated the Window Invalid Mask Register

--- wim.v
+++ wim.v
@@ -1,5 +1,5 @@
-module wim (output reg [31:0] out, input wire [31:0] in, input wire Clk);
-always @ (posedge Clk)
-	out <= 32'h0000000F;
+module wim (output reg [31:0] out, input wire [31:0] in, input wire enable, Clr, Clk);
+always @ (posedge Clk, posedge Clr)
+	if (Clr) out[3:0] <= 1'h0;
+	else if (enable) out[3:0] <= in[3:0];
 endmodule
-
