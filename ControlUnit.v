6ce5dcf4c8ed945672ff85e68b6107d3d1550194
Carlos N. Abreu Takemura <currysensei@outlook.com>
2014-10-18 00:06:33 -0400

    Fixed output reg derp

--- ControlUnit.v
+++ ControlUnit.v
@@ -1,15 +1,15 @@
 module ControlUnit(
 	// Enables
-	output NPC_enable, PC_enable, IR_Enable, MDR_Enable, MAR_Enable, register_file, RAM_enable, PSR_Enable,
+	output reg NPC_enable, PC_enable, IR_Enable, MDR_Enable, MAR_Enable, register_file, RAM_enable, PSR_Enable,
 	// Select Lines Muxes
-	output [1:0]extender_select, [1:0]ALUB_Mux_select,
-	output MDR_Mux_select,
+	output reg [1:0]extender_select, [1:0]ALUB_Mux_select,
+	output reg MDR_Mux_select,
 	// Register file input
-	output [4:0]in_PC, [4:0]in_PA, [4:0]in_PB,
+	output reg [4:0]in_PC, [4:0]in_PA, [4:0]in_PB,
 	// Alu inputs
-	output [5:0]ALU_op,
+	output reg [5:0]ALU_op,
 	// Ram input
-	output [5:0]RAM_OpCode,
+	output reg [5:0]RAM_OpCode,
 	
 	input [31:0]IR_Out,
 	input MFC);
