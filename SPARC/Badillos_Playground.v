4efcdede21ba9b12b30e4d36261f39ff59add6fe
Edwin O. Badillo <edwin.o.badillo@ieee.org>
2014-11-29 15:36:13 -0400

    Badillos playground updates

--- SPARC/Badillos_Playground.v
+++ SPARC/Badillos_Playground.v
@@ -711,33 +711,106 @@ module ControlUnit(
 	PSR_Enable = 0;
 	end
 
-	//RDWIM
-	if(PSR_Mux_out[7]) begin
-	
-	
+	//Read WIM
+	if(PSR_Mux_out[7]) begin 
+		//go to trap 5
 	end
 	else begin
-	 //TRAP
+		ALU_op = 6'b000000;
+		in_PA = 5'b00000;
+		in_PC  = IR_Out[29:25];
+		ALUA_Mux_select = 2'b00;
+		ALUB_Mux_select = 4'b1000;
+		#5;
+		register_file =1;
+		#5;
+		register_file =0;
 	end
 	
-	//RDTBR
-	if(PSR_Mux_out[7]) begin
-	
-	
+	//Read TBR
+	if(PSR_Mux_out[7]) begin 
+		//go to trap 5
 	end
 	else begin
-	 //TRAP
+		ALU_op = 6'b000000;
+		in_PA = 5'b00000;
+		in_PC  = IR_Out[29:25];
+		ALUA_Mux_select = 2'b00;
+		ALUB_Mux_select = 4'b1001;
+		#5;
+		register_file =1;
+		#5;
+		register_file =0;
 	end
 	
+	//Read PSR
+	if(PSR_Mux_out[7]) begin 
+		//go to trap 5
+	end
+	else begin
+		ALU_op = 6'b000000;
+		in_PA = 5'b00000;
+		in_PC  = IR_Out[29:25];
+		ALUA_Mux_select = 2'b00;
+		ALUB_Mux_select = 4'b1010;
+		#5;
+		register_file =1;
+		#5;
+		register_file =0;
+	end
 	
+	//Write PSR
+	if(PSR_Mux_out[7]) begin 
+		//go to trap 5
+	end
+	else begin
+		ALU_op = 6'b000011; //XOR
+		in_PA = IR_Out[18:14]; // Get rs1
+		if (IR_Out[13]) begin 
+			//B is an immediate argument in IR
+			ALUB_Mux_select = 4'b0001; // Select output of sign extender as B for ALU
+			extender_select = 2'b00;  // Select 13bit to 32bit extender
+		end
+		else begin 
+			//B is a register
+			ALUB_Mux_select = 4'b0000;
+			in_PB = IR_Out[4:0];
+		end
+		//ALU_out  = what we want to write
+		if(ALU_out[] > 3) begin////YOYOYOYOYO
+			//go to trap 5
+		end
+		else begin
+			PSR_Mux_select = 3'b101; //ALU_out
+			#5;
+			PSR_Enable = 1;
+			#5;
+			PSR_Enable = 0;
+		end
+	end
 	
-	
-	
-	
-	
-	
-	
-	
+	//Write WIM
+	if(PSR_Mux_out[7]) begin 
+		//go to trap 5
+	end
+	else begin
+		ALU_op = 6'b000011; //XOR
+		in_PA = IR_Out[18:14]; // Get rs1
+		if (IR_Out[13]) begin 
+			//B is an immediate argument in IR
+			ALUB_Mux_select = 4'b0001; // Select output of sign extender as B for ALU
+			extender_select = 2'b00;  // Select 13bit to 32bit extender
+		end
+		else begin 
+			//B is a register
+			ALUB_Mux_select = 4'b0000;
+			in_PB = IR_Out[4:0];
+		end
+		#5; //not needed when in state
+		WIM_Out = 1;
+		#5;
+		WIM_Out = 0;
+	end
 	
 	
 	
